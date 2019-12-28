25ed1be1b2535b641e2c4767b8da1b31063b0b2f
Łukasz Adler <ckmkingkong@gmail.com>
2019-12-28 09:09:42 +0100

    Add controller support

---
+++ MojBudzet/Controllers/MonthlyBudgetController.cs
@@ -0,0 +1,40 @@
+﻿namespace MojBudzet.Controllers
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using MojBudzet.BussinessLayer.Application.Budget;
+
+    /// <summary>
+    /// Monthy budget endpoint.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MonthlyBudgetController : ControllerBase
+    {
+        private readonly BudgetService budgetService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MonthlyBudgetController"/> class.
+        /// </summary>
+        /// <param name="budgetService">Provide services to manage monthly budget.</param>
+        public MonthlyBudgetController(BudgetService budgetService)
+        {
+            this.budgetService = budgetService;
+        }
+
+        /// <summary>
+        /// Create new monthly budget.
+        /// </summary>
+        /// <param name="command">Request body</param>
+        /// <returns>Operaton result</returns>
+        [HttpPost]
+        public async Task<IActionResult> Post(AddMonthlyBudgetCommand command)
+        {
+            var result = await this.budgetService.AddMonthlyBudgetAsync(command);
+
+            var resourcePath = this.Url.Action("Get", "MonthlyBudget");
+
+            return this.Created(resourcePath, result);
+        }
+    }
+}
